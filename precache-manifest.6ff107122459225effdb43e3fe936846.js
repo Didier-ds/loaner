1c4925e7b91113f5304c84c0ab215527a5bc7ec9
Didier-ds <didiersenou32@gmail.com>
2021-11-05 19:08:26 +0100

    deploy

---
+++ precache-manifest.6ff107122459225effdb43e3fe936846.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cfc6b3ee59030577510bff38fac8c5b",
+    "url": "/404.html"
+  },
+  {
+    "revision": "78a6127203d8c111345f",
+    "url": "/css/app.cf1abd35.css"
+  },
+  {
+    "revision": "dcf9e66f74a2d2e9249999fc091b147b",
+    "url": "/img/Face.dcf9e66f.png"
+  },
+  {
+    "revision": "4b874bd10d924d782d5ed5acd9e7e227",
+    "url": "/img/add_btn.4b874bd1.png"
+  },
+  {
+    "revision": "055649ae05ee033c10042367a9e8705a",
+    "url": "/img/desktop_logo.055649ae.svg"
+  },
+  {
+    "revision": "14e92d2cb1fd2a22a7cfddd653e0e730",
+    "url": "/img/mobile_logo.14e92d2c.svg"
+  },
+  {
+    "revision": "a6d2a3e4660b5745c3d63f3c941641f2",
+    "url": "/img/portfolio_card.svg"
+  },
+  {
+    "revision": "27f3c06fcbe6b8ec1b2c502407eef072",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3e70fdd06008233dea5",
+    "url": "/js/about.6d272029.js"
+  },
+  {
+    "revision": "78a6127203d8c111345f",
+    "url": "/js/app.d026fdcb.js"
+  },
+  {
+    "revision": "9a89cd9d882de1e0e55b",
+    "url": "/js/chunk-vendors.5db364b6.js"
+  },
+  {
+    "revision": "f5c680a277c7d8075ebce98e20201dbc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
